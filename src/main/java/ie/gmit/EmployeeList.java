f6fd572ba770582f7c8d78b4f0856a140446eed1
Sergey <g00354709@gmit.ie>
2021-03-10 10:28:01 +0000

    bug fixes

--- src/main/java/ie/gmit/EmployeeList.java
+++ src/main/java/ie/gmit/EmployeeList.java
@@ -11,14 +11,31 @@ public class EmployeeList {
 
     public void addToList(Employee e){
         if (e.verifyAllFieldsSet()) {
-            list.add(e);
+            if(list.size() > 0)
+            {
+               if (verifyUniqueInput(e))
+               {
+                   list.add(e);
+               } else throw new IllegalArgumentException("This employee class is already in the list");
+            } else{
+                list.add(e);
+            }
         } else throw new IllegalArgumentException("Some Fields didnt set correctly");
     }
 
     public List<Employee> getList() {
         return list;
     }
-
+    public int getListSize(){
+        return list.size();
+    }
+    public boolean verifyUniqueInput(Employee e){
+        for(int i = 0; i < list.size(); i++){
+            if(e.equals(list.get(i))){
+                return false;
+            }
+        }
+        return true;
+    }
 }
-
 
--- src/test/java/ie/gmit/Test.java
+++ src/test/java/ie/gmit/Test.java
@@ -67,10 +67,40 @@ public class Test {
             @Override
             public void execute() throws Throwable {
                 Employee e1 = new Employee("Sergey", "88888888", "55555", 18);
+                Employee e2 = new Employee("hello", "44444444", "77777", 20);
                 EmployeeList el = new EmployeeList();
                 el.addToList(e1);
+                el.addToList(e2);
             }
         };
         assertDoesNotThrow(ex);
+    }
+    @org.junit.jupiter.api.Test
+    void testVerifyNotAddingToList(){
+        Executable ex = new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                Employee e2 = new Employee("gh", "4444", "77", 15);
+                EmployeeList el = new EmployeeList();
+                el.addToList(e2);
+            }
+        };
+        assertThrows(IllegalArgumentException.class, ex);
+    }
+    @org.junit.jupiter.api.Test
+    void testVerifyNotAddingSameClasses(){
+        Executable ex = new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                Employee e1 = new Employee("Sergey", "88888888", "55555", 18);
+                Employee e2 = new Employee("dfgdfg", "99999999", "666666", 20);
+                Employee e3 = new Employee("Sergey", "88888888", "55555", 22);
+                EmployeeList el = new EmployeeList();
+                el.addToList(e1);
+                el.addToList(e2);
+                el.addToList(e3);
+            }
+        };
+        assertThrows(IllegalArgumentException.class, ex);
     }
 }
